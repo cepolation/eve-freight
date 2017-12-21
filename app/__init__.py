54b572b72b371c7a7900b38ed8c9282638e5f348
Kaleb Porter <porowns@gmail.com>
2017-12-21 13:55:14 -0500

    Added additional information needed for ESI, now just need to build callback url

--- app/__init__.py
+++ app/__init__.py
@@ -14,5 +14,26 @@ app.config['ESI_SECRET_KEY'] = "fEL01TRwKRZKGsW48bnpNm3TInKcK7o5hmjsW7vS"
 app.config['ESI_SCOPES'] = "esi-contracts.read_corporation_contracts.v1"
 app.config['ESI_CALLBACK_URL'] = "http://localhost:5000/esi/callback"
 
+## ESI
+from esipy import App as esiapp
+from esipy import EsiClient, EsiSecurity
+esi_app = esiapp.create(url="https://esi.tech.ccp.is/latest/swagger.json?datasource=tranquility")
+esi_client = EsiClient(
+        retry_requests=True,
+        header={'User-Agent': 'Something CCP can use to contact you and that define your app'},
+        raw_body_only=False,
+        )
+esi_security = EsiSecurity(
+        app=esi_app,
+        redirect_uri='https://localhost/esi/callback',
+        client_id=app.config['ESI_CLIENT_ID'],
+        secret_key=app.config['ESI_SECRET_KEY'],
+        )
+client=EsiClient(security=esi_security)
+app.config['AUTH_URL'] = esi_security.get_auth_uri(
+        scopes=app.config['ESI_SCOPES'],
+        state="None"
+        )
+
 app.config['SQLALCHEMY_DATABASE_URI']='mysql+pymysql://' + app.config['SQL_USER'] + ':' + app.config['SQL_PASSWORD'] + '@localhost/dev'
 db = SQLAlchemy(app)
