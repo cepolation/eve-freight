432ef6584e3b24352072832bfd8cd82cf081fb53
porowns <porowns@gmail.com>
2017-12-25 07:49:52 -0500

    Added callback for ESI route

--- app/__init__.py
+++ app/__init__.py
@@ -5,13 +5,13 @@ from app.eve.views import *
 
 app = Flask(__name__)
 app.register_blueprint(main, url_prefix='/')
-app.register_blueprint(eve, url_prefix='/esi')
+app.register_blueprint(eve, url_prefix='/eve')
 ## CONFIG
 app.config['SQL_USER'] = "root"
 app.config['SQL_PASSWORD'] = "password"
 app.config['ESI_CLIENT_ID'] = "d9e042c7462046018b867637ba6fe1cd"
 app.config['ESI_SECRET_KEY'] = "fEL01TRwKRZKGsW48bnpNm3TInKcK7o5hmjsW7vS"
-app.config['ESI_CALLBACK_URL'] = "http://159.203.85.204:5000/esi/callback"
+app.config['ESI_CALLBACK_URL'] = "http://127.0.0.1:5000/eve/callback"
 
 ## ESI
 from esipy import App as esiapp
@@ -33,6 +33,8 @@ app.config['ESI_REDIRECT_URL'] = esi_security.get_auth_uri(
         scopes=['esi-contracts.read_corporation_contracts.v1'],
         state="None"
         )
+app.config['esi_security'] = esi_security
+app.config['esi_client'] = esi_client
 
 app.config['SQLALCHEMY_DATABASE_URI']='mysql+pymysql://' + app.config['SQL_USER'] + ':' + app.config['SQL_PASSWORD'] + '@localhost/dev'
 db = SQLAlchemy(app)
--- app/eve/views.py
+++ app/eve/views.py
@@ -1,7 +1,23 @@
-from flask import render_template, Blueprint, redirect
+from flask import render_template, Blueprint, redirect, request
 from flask import current_app as app
+from app.utils.list import *
 eve = Blueprint('eve', __name__)
 
 @eve.route('/')
 def index():
     return redirect(app.config['ESI_REDIRECT_URL'])
+@eve.route('/callback')
+def callback():
+    code = request.args.get('code')
+    token = request.args.get('state')
+    print(code)
+    print(token)
+    esi_response = app.config['esi_security'].auth(code)
+    user = app.config['esi_security'].verify()
+    app.config['user'] = user
+    print(app.config['user'])
+    print(user)
+    return redirect('/')
+
+
+
