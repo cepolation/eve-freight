a7a7d67303c5ec7a1c8b588e021fa5ac48c829f7
AlbertoRFer <albertorf@instec.cu>
2018-02-16 19:43:00 +0100

    Basic contracts display in main page.
    - Parse of the contract useful information into a list in the main/views.
    - Display the parsed info in a table in the main page.

--- app/main/views.py
+++ app/main/views.py
@@ -13,6 +13,25 @@ def contracts():
         corporation_id=app.config['CORPORATION_ID']
     )
     contracts = app.config['esi_client'].request(op)
+
+    show_data = []
+    for contract in contracts.data:
+        if app.config['CORPORATION_ID'] != contract['assignee_id']:
+            continue
+
+        op = esi_app.op['get_characters_character_id'](
+            character_id=contract['issuer_id']
+        )
+        issuer = app.config['esi_client'].request(op).data['name']
+        iss_time = contract['date_issued']
+        exp_time = contract['date_expired']
+        volume = str(contract['volume']) + " m3"
+        show_data.append((issuer, iss_time, exp_time, volume))
+
+    op = esi_app.op['get_corporations_corporation_id'](
+        corporation_id=app.config['CORPORATION_ID']
+    )
+    corp = app.config['esi_client'].request(op).data['name']
     print(contracts)
     print(contracts.data)
-    return render_template('main.html')
+    return render_template('main.html', data=show_data, corp=corp)
